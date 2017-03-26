7d7eadd4e834d3b366f72bbfa8ee693f70690a3a
Forelax <helloworldmkv@163.com>
2017-03-26 22:14:36 +0800

    change routes test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,26 +7,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select 'title', 'Home'
   end
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select 'title', 'Home'
-  end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
     assert_select 'title', 'Help'
   end
 
   test "should get about" do
-    get static_pages_about_path
+    get about_url
     assert_response :success
     assert_select 'title', 'About'
   end
 
   test "should get contact" do
-    get static_pages_contact_path
+    get contact_url
     assert_response :success
     assert_select 'title', 'Contact'
   end
