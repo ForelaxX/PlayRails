84c5a5d0cea98d406c9a84aa3f64d411cb3a8ea5
Forelax <helloworldmkv@163.com>
2017-04-02 15:50:16 +0800

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/signup', to: 'user#new'
+  get '/signup', to: 'users#new'
 
   get 'help', to: 'static_pages#help'
 
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'contact', to: 'static_pages#contact'
 
   root 'static_pages#home'
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,28 +6,28 @@ class UserTest < ActiveSupport::TestCase
         password: 'foobar', password_confirmation: 'foobar'
   end
 
-  test "user name should be present" do
+  test "users name should be present" do
     @user.name = ''
     assert_not @user.valid?
     end
 
-  test "user email should be present" do
+  test "users email should be present" do
     @user.email = ''
     assert_not @user.valid?
   end
 
-  test "user name can not be too long" do
+  test "users name can not be too long" do
     @user.name = '#' * 21
     assert_not @user.valid?
   end
 
-  test "user email can not be too long" do
+  test "users email can not be too long" do
     @user.name = '#' * 44 + '@163.com'
     assert_not @user.valid?
   end
 
-  test "user email should in valid format" do
-    valid_mails = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+  test "users email should in valid format" do
+    valid_mails = %w[users@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
     valid_mails.each do |valid_mail|
       @user.email = valid_mail
       assert @user.valid?, "#{@user.email} should be valid"
@@ -41,19 +41,19 @@ class UserTest < ActiveSupport::TestCase
     assert_not another_user.valid?
   end
 
-  test "user email should be saved as lowercase" do
+  test "users email should be saved as lowercase" do
     upper_email = "EMAMPLE@qq.com"
     @user.email = upper_email
     @user.save
     assert_equal @user.reload.email, upper_email.downcase
   end
 
-  test 'user passwd should be present' do
+  test 'users passwd should be present' do
     @user.password = @user.password_confirmation = ''
     assert_not @user.valid?
   end
 
-  test 'user passwd should have minimum length' do
+  test 'users passwd should have minimum length' do
     @user.password = @user.password_confirmation = '1' * 5
     assert_not @user.valid?
   end
