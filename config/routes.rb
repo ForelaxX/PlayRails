51ea9b1ba900471795457d9b97b644eea1f913be
Forelax <helloworldmkv@163.com>
2017-04-03 10:27:55 +0800

    change erb to haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get '/signup', to: 'users#new'
 
   get 'help', to: 'static_pages#help'
@@ -7,6 +9,12 @@ Rails.application.routes.draw do
   get 'about', to: 'static_pages#about'
 
   get 'contact', to: 'static_pages#contact'
+
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
 
   root 'static_pages#home'
 
